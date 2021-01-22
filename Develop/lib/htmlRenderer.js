4b201b0bed25ff8062c8e3b9d804087cccd6bc2e
BtramVu <bbbvut@gmail.com>
2021-01-22 00:03:16 -0800

    add prompts questions

--- Develop/lib/htmlRenderer.js
+++ Develop/lib/htmlRenderer.js
@@ -18,7 +18,7 @@ const render = employees => {
     .filter(employee => employee.getRole() === "Intern")
     .map(intern => renderIntern(intern))
   );
-
+  console.log(html);
   return renderMain(html.join(""));
 
 };
